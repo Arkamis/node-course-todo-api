6293577f4d367e0958a3d36913b275546d417b4f
Miguel Rodriguez <miguelantonio_rodriguez@ucol.mx>
2019-02-03 20:33:05 -0600

    Test to /GET Added

--- server/server.js
+++ server/server.js
@@ -16,11 +16,19 @@ app.post('/todos', (req, res) => {
 
   todo.save().then((doc) => {
     res.send(doc);
-  }, (e) => {
+  }).catch( (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then(todos => {
+    res.send({todos});
+  }).catch( e => res.send(e));
 });
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {app};
